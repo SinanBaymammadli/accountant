5d9eed39989be3406973c3205c1e5281db107d8e
Sinan Baymammadli <sinan.baymammadli@gmail.com>
2019-05-01 10:58:49 +0400

    client show fix

--- lib/screens/client/client_show.dart
+++ lib/screens/client/client_show.dart
@@ -1,4 +1,10 @@
+import 'package:accountant/helpers/dateFormatter.dart';
 import 'package:accountant/models/client.dart';
+import 'package:accountant/models/order.dart';
+import 'package:accountant/models/payment.dart';
+import 'package:accountant/models/product.dart';
+import 'package:accountant/screens/order/order_show.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ClientShowScreen extends StatefulWidget {
@@ -31,11 +37,164 @@ class _ClientShowScreenState extends State<ClientShowScreen> {
         ),
         body: TabBarView(
           children: [
-            Text('odenisler'),
-            Text('alqi'),
+            StreamBuilder<QuerySnapshot>(
+              stream: Firestore.instance
+                  .collection('payments')
+                  .where('client_ref', isEqualTo: widget.client.ref)
+                  .orderBy('date', descending: true)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData)
+                  return Center(child: CircularProgressIndicator());
+
+                return _buildPaymentList(context, snapshot.data.documents);
+              },
+            ),
+            StreamBuilder<QuerySnapshot>(
+              stream: Firestore.instance
+                  .collection('orders')
+                  .where('client_ref', isEqualTo: widget.client.ref)
+                  .orderBy('date', descending: true)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData)
+                  return Center(child: CircularProgressIndicator());
+
+                return _buildOrderList(context, snapshot.data.documents);
+              },
+            ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildPaymentList(
+      BuildContext context, List<DocumentSnapshot> documents) {
+    if (documents.length == 0) {
+      return Center(
+        child: Text('Ödəmə yoxdur'),
+      );
+    }
+
+    return ListView.separated(
+      itemCount: documents.length,
+      separatorBuilder: (BuildContext context, int index) => Divider(),
+      itemBuilder: (BuildContext context, int index) {
+        DocumentSnapshot document = documents[index];
+
+        return _buildPaymentListItem(context, document);
+      },
+    );
+  }
+
+  Widget _buildPaymentListItem(
+      BuildContext context, DocumentSnapshot document) {
+    final payment = Payment.fromSnapshot(document);
+
+    return ListTile(
+      leading: payment.toUs
+          ? Icon(
+              Icons.arrow_downward,
+              color: Colors.green,
+            )
+          : Icon(
+              Icons.arrow_upward,
+              color: Colors.red,
+            ),
+      title: Text('${payment.amount} AZN'),
+      subtitle: Text(formatDate(payment.date)),
+      trailing: StreamBuilder<DocumentSnapshot>(
+        stream: payment.clientRef.snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData || !snapshot.data.exists) {
+            return Text('');
+          }
+
+          Client client = Client.fromSnapshot(snapshot.data);
+
+          return Text(client.name);
+        },
+      ),
+    );
+  }
+
+  Widget _buildOrderList(
+      BuildContext context, List<DocumentSnapshot> documents) {
+    if (documents.length == 0) {
+      return Center(
+        child: Text('Alqi-satgi yoxdur'),
+      );
+    }
+
+    return ListView.separated(
+      itemCount: documents.length,
+      separatorBuilder: (BuildContext context, int index) => Divider(),
+      itemBuilder: (BuildContext context, int index) {
+        DocumentSnapshot document = documents[index];
+        return _buildOrderListItem(context, document);
+      },
+    );
+  }
+
+  Widget _buildOrderListItem(BuildContext context, DocumentSnapshot document) {
+    final order = Order.fromSnapshot(document);
+
+    print('here: ${order.productRef.toString()}');
+
+    return ListTile(
+      leading: order.isBuy
+          ? Icon(
+              Icons.arrow_downward,
+              color: Colors.green,
+            )
+          : Icon(
+              Icons.arrow_upward,
+              color: Colors.red,
+            ),
+      title: StreamBuilder<DocumentSnapshot>(
+        stream: order.clientRef.snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData || !snapshot.data.exists) {
+            return Text('');
+          }
+
+          Client client = Client.fromSnapshot(snapshot.data);
+
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(client.name),
+              Text(formatDate(order.date)),
+            ],
+          );
+        },
+      ),
+      subtitle: StreamBuilder<DocumentSnapshot>(
+        stream: order.productRef.snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData || !snapshot.data.exists) {
+            return Text('');
+          }
+
+          Product product = Product.fromSnapshot(snapshot.data);
+
+          return Text(
+              '${product.name} => ${order.productAmount} kq * ${order.productPrice} AZN = ${(order.productAmount * order.productPrice).round()} AZN');
+        },
+      ),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (BuildContext context) {
+              return OrderShowScreen(
+                order: order,
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
